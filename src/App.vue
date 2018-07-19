527b93fcf938f6b606d5927e0e8e9ebf51d89eb7
Yoshiki <rrrtcdish2@gmail.com>
2018-07-20 08:59:56 +0900

    wip Contact.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <md-tab id="tab-home" md-icon="home" md-label="top" to="/top"></md-tab>
       <md-tab id="tab-pages" md-icon="play_arrow" md-label="story" to="/story"></md-tab>
       <md-tab id="tab-posts" md-icon="pages" md-label="Posts" to="/post"></md-tab>
-      <md-tab id="tab-favorites" md-icon="contacts" md-label="Contact"></md-tab>
+      <md-tab id="tab-favorites" md-icon="contacts" md-label="Contact" to="/contact"></md-tab>
     </md-tabs>
 
     <router-view/>
