dbc1ef690107d923033c0a98cf32585048623746
Yoshiki <rrrtcdish2@gmail.com>
2018-07-20 23:23:19 +0900

    Update Contact.vue and refact UI

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,167 +1,159 @@
 <template>
   <div>
     <form novalidate class="md-layout" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-50 md-small-size-100">
-        <md-card-header>
-          <div class="md-title">Users</div>
-        </md-card-header>
+      <div class="md-layout md-alignment-center-center">
+        <md-card class="md-layout-item md-size-80 ">
+          <md-card-header>
+            <div class="md-title">Contact for me with message!</div>
+          </md-card-header>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('firstName')">
-                <label for="first-name">First Name</label>
-                <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.firstName" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.firstName.required">The first name is required</span>
-                <span class="md-error" v-else-if="!$v.form.firstName.minlength">Invalid first name</span>
-              </md-field>
+          <md-card-content>
+            <div class="md-layout md-gutter">
+              <div class="md-layout-item md-small-size-100">
+                <md-field :class="getValidationClass('YourName')">
+                  <label for="first-name">Your Name</label>
+                  <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.YourName" :disabled="sending" />
+                  <span class="md-error" v-if="!$v.form.YourName.required">Your name is required</span>
+                  <span class="md-error" v-else-if="!$v.form.YourName.minlength">Invalid first name</span>
+                </md-field>
+              </div>
             </div>
 
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('lastName')">
-                <label for="last-name">Last Name</label>
-                <md-input name="last-name" id="last-name" autocomplete="family-name" v-model="form.lastName" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.lastName.required">The last name is required</span>
-                <span class="md-error" v-else-if="!$v.form.lastName.minlength">Invalid last name</span>
-              </md-field>
+            <div class="md-layout md-gutter">
+              <div class="md-layout-item md-small-size-100">
+                <md-field :class="getValidationClass('gender')">
+                  <label for="gender">Gender</label>
+                  <md-select name="gender" id="gender" v-model="form.gender" md-dense :disabled="sending">
+                    <md-option value="M">M</md-option>
+                    <md-option value="F">F</md-option>
+                  </md-select>
+                  <span class="md-error">The gender is required</span>
+                </md-field>
+              </div>
+
+              <div class="md-layout-item md-small-size-100">
+                <md-field :class="getValidationClass('age')">
+                  <label for="age">Age</label>
+                  <md-input type="number" id="age" name="age" autocomplete="age" v-model="form.age" :disabled="sending" />
+                  <span class="md-error" v-if="!$v.form.age.required">The age is required</span>
+                  <span class="md-error" v-else-if="!$v.form.age.maxlength">Invalid age</span>
+                </md-field>
+              </div>
             </div>
-          </div>
 
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('gender')">
-                <label for="gender">Gender</label>
-                <md-select name="gender" id="gender" v-model="form.gender" md-dense :disabled="sending">
-                  <md-option></md-option>
-                  <md-option value="M">M</md-option>
-                  <md-option value="F">F</md-option>
-                </md-select>
-                <span class="md-error">The gender is required</span>
-              </md-field>
-            </div>
+            <md-field :class="getValidationClass('email')">
+              <label for="email">Email</label>
+              <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+              <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+              <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
+            </md-field>
 
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('age')">
-                <label for="age">Age</label>
-                <md-input type="number" id="age" name="age" autocomplete="age" v-model="form.age" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.age.required">The age is required</span>
-                <span class="md-error" v-else-if="!$v.form.age.maxlength">Invalid age</span>
-              </md-field>
-            </div>
-          </div>
+            <md-field>
+              <label>Message</label>
+              <md-textarea v-model="message"></md-textarea>
+            </md-field>
+          </md-card-content>
 
-          <md-field :class="getValidationClass('email')">
-            <label for="email">Email</label>
-            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
-            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
-          </md-field>
-        </md-card-content>
+          <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+          <md-card-actions>
+            <md-button type="submit" class="md-primary" :disabled="sending">Send message</md-button>
+          </md-card-actions>
+        </md-card>
+      </div>
 
-        <md-card-actions>
-          <md-button type="submit" class="md-primary" :disabled="sending">Create user</md-button>
-        </md-card-actions>
-      </md-card>
-
-      <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
+      <md-snackbar :md-active.sync="messageSend">Sending is success! Thank you!</md-snackbar>
     </form>
   </div>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import {
-    required,
-    email,
-    minLength,
-    maxLength
-  } from 'vuelidate/lib/validators'
+import { validationMixin } from 'vuelidate'
+import {
+  required,
+  email,
+  minLength,
+  maxLength
+} from 'vuelidate/lib/validators'
 
-  export default {
-    name: 'FormValidation',
-    mixins: [validationMixin],
-    data: () => ({
-      form: {
-        firstName: null,
-        lastName: null,
-        gender: null,
-        age: null,
-        email: null,
+export default {
+  name: 'FormValidation',
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      YourName: null,
+      gender: null,
+      age: null,
+      email: null
+    },
+    messageSend: false,
+    sending: false,
+    userName: null
+  }),
+  validations: {
+    form: {
+      YourName: {
+        required,
+        minLength: minLength(3)
       },
-      userSaved: false,
-      sending: false,
-      lastUser: null
-    }),
-    validations: {
-      form: {
-        firstName: {
-          required,
-          minLength: minLength(3)
-        },
-        lastName: {
-          required,
-          minLength: minLength(3)
-        },
-        age: {
-          required,
-          maxLength: maxLength(3)
-        },
-        gender: {
-          required
-        },
-        email: {
-          required,
-          email
+      age: {
+        required,
+        maxLength: maxLength(3)
+      },
+      gender: {
+        required
+      },
+      email: {
+        required,
+        email
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
         }
       }
     },
-    methods: {
-      getValidationClass (fieldName) {
-        const field = this.$v.form[fieldName]
+    clearForm () {
+      this.$v.$reset()
+      this.form.YourName = null
+      this.form.lastName = null
+      this.form.age = null
+      this.form.gender = null
+      this.form.email = null
+    },
+    saveUser () {
+      this.sending = true
 
-        if (field) {
-          return {
-            'md-invalid': field.$invalid && field.$dirty
-          }
-        }
-      },
-      clearForm () {
-        this.$v.$reset()
-        this.form.firstName = null
-        this.form.lastName = null
-        this.form.age = null
-        this.form.gender = null
-        this.form.email = null
-      },
-      saveUser () {
-        this.sending = true
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.userName = `${this.form.YourName}`
+        this.messageSend = true
+        this.sending = false
+        this.clearForm()
+      }, 1500)
+    },
+    validateUser () {
+      this.$v.$touch()
 
-        // Instead of this timeout, here you can call your API
-        window.setTimeout(() => {
-          this.lastUser = `${this.form.firstName} ${this.form.lastName}`
-          this.userSaved = true
-          this.sending = false
-          this.clearForm()
-        }, 1500)
-      },
-      validateUser () {
-        this.$v.$touch()
-
-        if (!this.$v.$invalid) {
-          this.saveUser()
-        }
+      if (!this.$v.$invalid) {
+        this.saveUser()
       }
     }
   }
+}
 </script>
 
 <style scoped>
-  .md-progress-bar {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-  }
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
 </style>
