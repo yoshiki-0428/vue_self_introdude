d908b43f8eb6e97856c79472e4def6a9a985922f
rrrtcdish2 <rrrtcdish2@gmail.com>
2018-10-16 16:02:06 +0900

    Refactor & Handle resize

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -8,58 +8,27 @@
 import * as THREE from 'three'
 
 export default {
-
   name: 'backGround',
   data () {
-    // === scene ===
-    const scene = new THREE.Scene()
-
-    // === renderer ===
-    const renderer = new THREE.WebGLRenderer({alpha: true})
-    renderer.setSize(window.innerWidth, window.innerHeight - 160)
-
-    // === camera ===
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight - 160), 0.1, 1000)
-    camera.position.z = 5
-
-    // === light ===
-    const light = new THREE.DirectionalLight(0xffffff)
-    light.position.set(0, 0, 10)
-
-    // === model ===
-    const cubeSize = 2.5
-    const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
-    const material = new THREE.MeshNormalMaterial({color: 0x00ff00})
-
-    // === cubes ===
-    const cube = new THREE.Mesh(geometry, material)
-    const cubeVertical = new THREE.Mesh(geometry, material)
-    const cubeHorizon = new THREE.Mesh(geometry, material)
     return {
-      scene: scene,
-      renderer: renderer,
-      camera: camera,
-      light: light,
-      cube: cube,
-      cubeVertical: cubeVertical,
-      cubeHorizon: cubeHorizon
+      scene: this.scene,
+      renderer: this.renderer,
+      camera: this.camera,
+      light: this.light,
+      cube: this.cube,
+      cubeVertical: this.cubeVertical,
+      cubeHorizon: this.cubeHorizon
     }
   },
 
   created () {
-    // === sceneにmodel,light, cameraを追加 ===
-    this.scene.add(this.camera)
-    this.scene.add(this.light)
-    this.scene.add(this.cube)
-    this.scene.add(this.cubeVertical)
-    this.scene.add(this.cubeHorizon)
-    // === 初期位置の設定 ===
-    this.cube.position.set(0, 0.5, 0)
-    this.cubeVertical.position.set(0, 0.5, 0)
-    this.cubeHorizon.position.set(0, 0.5, 0)
+    this.initThree()
+    this.setThree()
     // === 画面全体のクリックイベントを設定 ===
     window.onclick = () => this.zoomCamera()
     window.setTimeout(this.moveCube, 5000)
+    // === リサイズ対応 ===
+    window.addEventListener('resize', this.onResize)
   },
 
   mounted () {
@@ -69,6 +38,44 @@ export default {
   },
 
   methods: {
+    initThree () {
+      // === scene ===
+      this.scene = new THREE.Scene()
+
+      // === renderer ===
+      this.renderer = new THREE.WebGLRenderer({alpha: true})
+      this.renderer.setSize(window.innerWidth, window.innerHeight - 160)
+
+      // === camera ===
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight - 160), 0.1, 1000)
+      this.camera.position.z = 5
+
+      // === light ===
+      this.light = new THREE.DirectionalLight(0xffffff)
+      this.light.position.set(0, 0, 10)
+
+      // === model ===
+      const cubeSize = 2.5
+      const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
+      this.material = new THREE.MeshNormalMaterial({color: 0x00ff00})
+
+      // === cubes ===
+      this.cube = new THREE.Mesh(geometry, this.material)
+      this.cubeVertical = new THREE.Mesh(geometry, this.material)
+      this.cubeHorizon = new THREE.Mesh(geometry, this.material)
+    },
+    setThree () {
+      // === sceneにmodel,light, cameraを追加 ===
+      this.scene.add(this.camera)
+      this.scene.add(this.light)
+      this.scene.add(this.cube)
+      this.scene.add(this.cubeVertical)
+      this.scene.add(this.cubeHorizon)
+      // === 初期位置の設定 ===
+      this.cube.position.set(0, 0.5, 0)
+      this.cubeVertical.position.set(0, 0.5, 0)
+      this.cubeHorizon.position.set(0, 0.5, 0)
+    },
     animate () {
       // 再帰により回転動作を繰り返す
       requestAnimationFrame(this.animate)
@@ -115,6 +122,18 @@ export default {
         this.moveCube()
       }
       this.renderer.render(this.scene, this.camera)
+    },
+    onResize () {
+      // サイズを取得
+      const width = window.innerWidth
+      const height = window.innerHeight
+      // レンダラーのサイズを調整する
+      this.renderer.setPixelRatio(window.devicePixelRatio)
+      this.renderer.setSize(width, height)
+      // カメラのアスペクト比を正す
+      this.camera.aspect = width / height
+      this.camera.updateProjectionMatrix()
+      console.log('resize!!')
     }
   }
 }
