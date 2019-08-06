6f88036e75e44de1160c6a02237dd57553296da6
yoshiki-0428 <rrrtcdish2@gmail.com>
2019-08-06 17:55:50 +0900

    認証情報の追加

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "introduction-my-web-site.firebaseapp.com",
+  databaseURL: "https://introduction-my-web-site.firebaseio.com",
+  projectId: "introduction-my-web-site",
+  storageBucket: "introduction-my-web-site.appspot.com",
+  messagingSenderId: "198759574534"
+};
